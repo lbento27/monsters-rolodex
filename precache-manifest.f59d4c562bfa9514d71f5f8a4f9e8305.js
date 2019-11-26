2cc8d22fa74eb0479ce64f8ebc6bfc2c0e3230bb
Bento <lbento525@gmail.com>
2019-11-26 23:25:33 +0000

    Updates

---
+++ precache-manifest.f59d4c562bfa9514d71f5f8a4f9e8305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ea60c7f9be84267b395b828e7be5d9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8e074b7f084436e8ecf8",
+    "url": "/monsters-rolodex/static/css/main.97b1b9ba.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "8e074b7f084436e8ecf8",
+    "url": "/monsters-rolodex/static/js/main.83c36331.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
