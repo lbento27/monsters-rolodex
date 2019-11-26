c673fe68fd4417fb1e478bd28d44bcd509b92984
Bento <lbento525@gmail.com>
2019-11-26 01:25:05 +0000

    monster-rolodex(React) project first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./component/card-list/card-list.component.jsx"; //import component made by me
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  //Component Life cycle are methods that get call at different stages(auto by react), when this component gets render
+  componentDidMount() {
+    //fetch makes a api call and returns a promise, note we can catch error if we want ex: .catch(err => console.log("Error!"));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json()) //taking the response and converting to json format, because its a promise that response will be raped in a new resolve promise so that way we can call .then
+      .then(users => this.setState({ monsters: users })); //set our monsters to that response array of users
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* use component made by me and pass truth props and props.children */}
+        <CardList monsters={this.state.monsters}>
+          {/* this will take everything in this CardList and passes in the component cardlist(props.children) given in this case a grid/style, note :now we move the code and pass state above */}
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Card } from "../card/card.component";
+
+import "./card-list.styles.css"; //import css
+// className"card-list" comes form the css
+export const CardList = props => {
+  return (
+    <div className="card-list">
+      {/* map() = iterate over an array, has a callback function thats call in every element and then returns a new array*/}
+      {props.monsters.map(monster => (
+        //monster.id its necessary because react needs to now in case of change one element witch element we are referring
+        //<h1 key={monster.id}>{monster.name}</h1>
+        <Card key={monster.id} monster={monster} /> //where we pass monster to the Card component
+      ))}
+    </div>
+  );
+};
